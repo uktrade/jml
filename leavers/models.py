9d2592742f0536d71deee0fa391514373d4c0b67
Ross Miller <github@decodedcollective.com>
2021-10-06 16:28:57 +0100

    Minor fixes

--- leavers/models.py
+++ leavers/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
 
-class Position(models.Model):
-    title = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.title
-
-
 class LeavingRequest(models.Model):
     requested_at = models.DateTimeField(auto_now_add=True)
     requested_by = models.CharField(max_length=255,)  # SSO id
