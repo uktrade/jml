00c9184a58a1e06bab0b6dcffbe4e45e62e1afb6
Ross Miller <github@decodedcollective.com>
2021-10-06 18:06:07 +0100

    Augment form function

--- leavers/forms.py
+++ leavers/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from leavers.models import LeavingRequest
 from leavers.widgets import DateSelectorWidget
 
@@ -25,3 +27,16 @@ class LeaversForm(forms.ModelForm):
         self.fields['for_self'].widget.attrs.update({'class': "govuk-checkboxes__input"})
         self.fields['leaver_email_address'].widget.attrs.update({'class': "govuk-textarea"})
         self.fields['last_day'].widget.attrs.update({'class': "govuk-textarea"})
+
+    def clean(self):
+        cleaned_data = super().clean()
+        for_self = cleaned_data.get("for_self")
+        leaver_email_address = cleaned_data.get("leaver_email_address")
+
+        if not for_self and not leaver_email_address:
+            raise ValidationError(
+                "If you are leaving the DIT, please check the 'I am "
+                "leaving DIT' checkbox. If you are filling this form "
+                "out for someone else please provide their email "
+                "address"
+            )
