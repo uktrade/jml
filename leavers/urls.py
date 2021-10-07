776cebe8ad6dfb9d44243c13b07288dc49ed5474
Ross Miller <github@decodedcollective.com>
2021-10-07 14:04:58 +0100

    Remove redundant views

--- leavers/urls.py
+++ leavers/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from django_workflow_engine import workflow_urls
 
 from leavers.views import (
-    leavers_form,
-    leaving_requests,
     LeaversFlowView,
     LeaversFlowListView,
     LeaversFlowCreateView,
@@ -12,8 +10,6 @@ from leavers.views import (
 )
 
 urlpatterns = [
-    path("", leaving_requests, name='leaving_requests'),
-    path("new/", leavers_form, name='leavers_form'),
     path(
         "workflow/",
         workflow_urls(
