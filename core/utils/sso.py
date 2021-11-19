a0ac009edd4a0615ff0eaab44366e5d55b06059b
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2021-11-19 12:30:00 +0000

    Update get_sso_user_details

--- core/utils/sso.py
+++ core/utils/sso.py
@@ -1,17 +1,27 @@
+from typing import TypedDict
 import requests
 from django.conf import settings
+from typing import List
 
 
-def get_sso_user_details(email):
-    # response = requests.get(
-    #     f"{settings.SSO_API_URL}/user/introspect/?TODO=query_params"
-    # )
-    #
-    # response.json()
+class SSOUserDetail(TypedDict):
+    first_name: str
+    last_name: str
+    sso_id: str
 
+
+def get_sso_user_details(*, email: str) -> SSOUserDetail:
+    """
+    Get user details from Staff SSO for a given email address
+    """
+    response = requests.get(
+        f"{settings.SSO_API_URL}/user/introspect/?email={email}"
+    )
+    # TODO: Check if the response was successful
+    # TODO: Check data output is correct
+    user = response.json()
     return {
-        # TODO - should return all SSO details
-        "first_name": "Test",
-        "last_name": "Test",
-        "sso_id": f"{email}._fake_sso_id",
+        "first_name": user["first_name"],
+        "last_name": user["last_name"],
+        "sso_id": user["user_id"],
     }
