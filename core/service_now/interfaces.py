fcf63be67d25c0e1a9b62a2a646e33f581c4ad10
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2021-11-24 11:44:16 +0000

    Add initial interface methods

--- core/service_now/interfaces.py
+++ core/service_now/interfaces.py
@@ -1,13 +1,53 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import List, TypedDict
+
+
+class AssetDetails(TypedDict):
+    # TODO: Define AssetDetails
+    pass
+
+
+class LineManagerDetails(TypedDict):
+    # TODO: Define LineManagerDetails
+    pass
+
+
+class LeaverRequestData(TypedDict):
+    # TODO: Define LeaverRequestData
+    pass
 
 
 class ServiceNowBase(ABC):
-    pass
+    @abstractmethod
+    def get_active_line_manages(self) -> List[LineManagerDetails]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_assets_for_user(self) -> List[AssetDetails]:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def submit_leaver_request(self, request_data: LeaverRequestData):
+        raise NotImplementedError
 
 
 class ServiceNowStubbed(ServiceNowBase):
-    pass
+    def get_assets_for_user(self) -> List[AssetDetails]:
+        return []
+
+    def get_active_line_manages(self) -> List[LineManagerDetails]:
+        return []
+
+    def submit_leaver_request(self, request_data: LeaverRequestData):
+        pass
 
 
 class ServiceNowInterface(ServiceNowBase):
-    pass
+    def get_assets_for_user(self) -> List[AssetDetails]:
+        return []
+
+    def get_active_line_manages(self) -> List[LineManagerDetails]:
+        return []
+
+    def submit_leaver_request(self, request_data: LeaverRequestData):
+        pass
