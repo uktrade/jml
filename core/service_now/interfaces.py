b81bcfe61e47306d7644f43e27feb38e142d3a52
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2021-11-24 13:03:39 +0000

    Define LeaverRequestData structure and fix typo

--- core/service_now/interfaces.py
+++ core/service_now/interfaces.py
@@ -1,5 +1,9 @@
+import logging
 from abc import ABC, abstractmethod
-from typing import List, TypedDict
+from datetime import date
+from typing import List, Optional, TypedDict
+
+logger = logging.getLogger(__name__)
 
 
 class AssetDetails(TypedDict):
@@ -12,14 +16,30 @@ class LineManagerDetails(TypedDict):
     pass
 
 
-class LeaverRequestData(TypedDict):
-    # TODO: Define LeaverRequestData
+class Address(TypedDict):
     pass
+
+
+class LeaverRequestData(TypedDict):
+    collection_address: Optional[Address]
+    collection_telephone: Optional[str]
+    collection_email: str
+    reason_for_leaving: str
+    leaving_date: date
+    employee_email: str
+    employee_name: str
+    employee_department: str
+    employee_directorate: str
+    employee_staff_id: str
+    manager_name: str
+    assets: List[AssetDetails]
+    assets_confirmation: bool
+    assets_information: str
 
 
 class ServiceNowBase(ABC):
     @abstractmethod
-    def get_active_line_manages(self) -> List[LineManagerDetails]:
+    def get_active_line_managers(self) -> List[LineManagerDetails]:
         raise NotImplementedError
 
     @abstractmethod
@@ -33,20 +53,23 @@ class ServiceNowBase(ABC):
 
 class ServiceNowStubbed(ServiceNowBase):
     def get_assets_for_user(self) -> List[AssetDetails]:
+        logger.info("Getting assets for a user")
         return []
 
-    def get_active_line_manages(self) -> List[LineManagerDetails]:
+    def get_active_line_managers(self) -> List[LineManagerDetails]:
+        logger.info("Getting active line managers")
         return []
 
     def submit_leaver_request(self, request_data: LeaverRequestData):
-        pass
+        logger.info("Submitting leaver request")
+        logger.info(request_data)
 
 
 class ServiceNowInterface(ServiceNowBase):
     def get_assets_for_user(self) -> List[AssetDetails]:
         return []
 
-    def get_active_line_manages(self) -> List[LineManagerDetails]:
+    def get_active_line_managers(self) -> List[LineManagerDetails]:
         return []
 
     def submit_leaver_request(self, request_data: LeaverRequestData):
