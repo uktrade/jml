83da425c421c5b01ff40e7a22410c7b12d0a222c
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2021-11-30 17:15:52 +0000

    Fix UserFactory

--- user/test/factories.py
+++ user/test/factories.py
@@ -1,6 +1,7 @@
 import uuid
 
 import factory
+from factory import fuzzy
 
 from user.models import User
 
@@ -9,9 +10,9 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
-    first_name = "Jane"
-    last_name = "Smith"
-    email = "jane.smith@test.com"
-    legacy_sso_user_id = uuid.uuid4()
-    username = "jane.smith@-1111111@id.test.gov.uk"
-    sso_contact_email = "jane.smith@test.com"
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    email = factory.Sequence(lambda n: f"user{n}@example.com")
+    legacy_sso_user_id = factory.Sequence(lambda n: f"SSO User ID {n}")
+    username = factory.Sequence(lambda n: f"user{n}")
+    sso_contact_email = factory.Sequence(lambda n: f"user{n}.contact@example.com")
