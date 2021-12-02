c50d3d9985d2122184e44907325321dc07149101
Ross Miller <github@decodedcollective.com>
2021-12-02 13:43:13 +0000

    Update logging config

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -23,28 +23,52 @@ LOGGING = {
         "ecs_formatter": {
             "()": ECSFormatter,
         },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
+        "simple": {
+            "format": "{asctime} {levelname} {message}",
+            "style": "{",
         },
     },
-    'handlers': {
-        'ecs': {
-            'class': 'logging.StreamHandler',
-            'stream': sys.stdout,
-            'formatter': 'ecs_formatter',
+    "handlers": {
+        "ecs": {
+            "class": "logging.StreamHandler",
+            "formatter": "ecs_formatter",
         },
-        'stdout': {
-            'class': 'logging.StreamHandler',
-            'stream': sys.stdout,
-            'formatter': 'simple',
-            'level': 'INFO',
+        "simple": {
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
         },
     },
-    'loggers': {
-        'django.request': {
-            'handlers': ['ecs', ],
-            'level': 'INFO',
-            'propagate': True,
+    "root": {
+        "handlers": [
+            "ecs",
+            "simple",
+        ],
+        "level": os.getenv("ROOT_LOG_LEVEL", "INFO"),  # noqa F405
+    },
+    "loggers": {
+        "django": {
+            "handlers": [
+                "ecs",
+                "simple",
+            ],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),  # noqa F405
+            "propagate": False,
+        },
+        "django.server": {
+            "handlers": [
+                "ecs",
+                "simple",
+            ],
+            "level": os.getenv("DJANGO_SERVER_LOG_LEVEL", "ERROR"),  # noqa F405
+            "propagate": False,
+        },
+        "django.db.backends": {
+            "handlers": [
+                "ecs",
+                "simple",
+            ],
+            "level": os.getenv("DJANGO_DB_LOG_LEVEL", "ERROR"),  # noqa F405
+            "propagate": False,
         },
     },
 }
