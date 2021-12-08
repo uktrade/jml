5cc37b199a255936486b1a4acc0e755469a0bfc3
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2021-12-08 16:33:30 +0000

    Add management command that keeps a local record of Staff SSO Activity Stream Users in the DB

---
+++ core/staff_sso/activity_stream.py
@@ -0,0 +1,83 @@
+from ctypes import Union
+from typing import Any, List, Literal, Optional, TypedDict
+
+import requests
+from django.conf import settings
+from mohawk import Receiver, Sender
+from mohawk.exc import HawkFail
+
+URL = settings.STAFF_SSO_ACTIVITY_STREAM_URL
+CONTENT_TYPE = "application/json"
+
+
+def get_sender(*, url: Optional[str] = None) -> Sender:
+    return Sender(
+        {
+            "id": settings.STAFF_SSO_ACTIVITY_STREAM_ID,
+            "key": settings.STAFF_SSO_ACTIVITY_STREAM_SECRET,
+            "algorithm": "sha256",
+        },
+        url,
+        "GET",
+        content="",
+        content_type=CONTENT_TYPE,
+    )
+
+
+class FailedToGetActivityStream(Exception):
+    pass
+
+
+OrderedItem = TypedDict(
+    "OrderedItem",
+    {
+        "dit:StaffSSO:User:becameInactiveOn": Optional[Any],
+        "dit:StaffSSO:User:contactEmailAddress": Optional[Any],
+        "dit:StaffSSO:User:emailUserId": str,
+        "dit:StaffSSO:User:joined": str,
+        "dit:StaffSSO:User:lastAccessed": str,
+        "dit:StaffSSO:User:permittedApplications": List[Any],
+        "dit:StaffSSO:User:status": str,
+        "dit:StaffSSO:User:userId": str,
+        "dit:emailAddress": str,
+        "dit:lastName": str,
+        "dit:firstName": str,
+        "id": str,
+        "name": str,
+        "type": str,
+    },
+)
+
+
+class ActivityStreamOrderedItems(TypedDict):
+    id: str
+    object: OrderedItem
+    published: str
+
+
+def get_activity_stream(
+    *,
+    url: Optional[str] = None,
+) -> List[ActivityStreamOrderedItems]:
+    if url is None:
+        url = URL
+    sender = get_sender(url=url)
+
+    response = requests.get(
+        url,
+        data="",
+        headers={"Authorization": sender.request_header, "Content-Type": CONTENT_TYPE},
+    )
+
+    if response.status_code != 200:
+        raise FailedToGetActivityStream()
+
+    data = response.json()
+    activity_stream = data.get("orderedItems", [])
+
+    # If there is a "next" URL, we need to recursively call this function unless
+    # the next URL is the current URL
+    if "next" in data and data["next"] != url:
+        activity_stream += get_activity_stream(url=data["next"])
+
+    return activity_stream
---
+++ core/staff_sso/management/commands/ingest_activity_stream.py
@@ -0,0 +1,62 @@
+from typing import List
+
+from django.core.management.base import BaseCommand
+
+from core.staff_sso import activity_stream, models
+
+
+class Command(BaseCommand):
+    help = "Ingest Staff SSO Activity Stream"
+
+    def handle(self, *args, **options):
+        created_updated_ids: List[int] = []
+
+        # Create and Update Activity Stream SSO objects
+        activity_stream_objects = activity_stream.get_activity_stream()
+        for activity_stream_object in activity_stream_objects:
+            # Only create objects with type of "dit:StaffSSO:User"
+            if activity_stream_object["object"]["type"] != "dit:StaffSSO:User":
+                continue
+
+            (
+                as_staff_sso_user,
+                _,
+            ) = models.ActivityStreamStaffSSOUser.objects.get_or_create(
+                identifier=activity_stream_object["object"]["id"],
+                defaults={
+                    "name": activity_stream_object["object"]["name"],
+                    "obj_type": activity_stream_object["object"]["type"],
+                    "first_name": activity_stream_object["object"]["dit:firstName"],
+                    "last_name": activity_stream_object["object"]["dit:lastName"],
+                    "email_address": activity_stream_object["object"][
+                        "dit:emailAddress"
+                    ],
+                    "user_id": activity_stream_object["object"][
+                        "dit:StaffSSO:User:userId"
+                    ],
+                    "status": activity_stream_object["object"][
+                        "dit:StaffSSO:User:status"
+                    ],
+                    "last_accessed": activity_stream_object["object"][
+                        "dit:StaffSSO:User:lastAccessed"
+                    ],
+                    "joined": activity_stream_object["object"][
+                        "dit:StaffSSO:User:joined"
+                    ],
+                    "email_user_id": activity_stream_object["object"][
+                        "dit:StaffSSO:User:emailUserId"
+                    ],
+                    "contact_email_address": activity_stream_object["object"][
+                        "dit:StaffSSO:User:contactEmailAddress"
+                    ],
+                    "became_inactive_on": activity_stream_object["object"][
+                        "dit:StaffSSO:User:becameInactiveOn"
+                    ],
+                },
+            )
+            created_updated_ids.append(as_staff_sso_user.id)
+
+        # Delete Activity Stream SSO objects that are no longer in the Activity Stream
+        models.ActivityStreamStaffSSOUser.objects.exclude(
+            id__in=created_updated_ids
+        ).delete()
