07f47d3adbc27fafe7ddf6dde3fe98302b8fce3e
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2021-12-13 12:15:35 +0000

    Remove Position
    Enable mypy in pre-commit
    
    Update requirements and cleanup dev settings
    
    Remove mypy (for now)

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,17 +1,7 @@
 import os
 import sys
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
 from config.settings.base import *  # type: ignore # noqa
-
-# TODO - add Sentry
-# sentry_sdk.init(
-#     os.environ.get("SENTRY_DSN"),
-#     environment=os.environ.get("SENTRY_ENVIRONMENT"),
-#     integrations=[DjangoIntegration()],
-# )
 
 INSTALLED_APPS += [  # type: ignore
     "dev_tools.apps.DevToolsConfig",
