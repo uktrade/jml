d16faf83fc2e56c5275fcc135ca30e0607ddbede
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2021-12-24 09:46:45 +0000

    Improving View tests

--- leavers/tests/views/test_leaving.py
+++ leavers/tests/views/test_leaving.py
@@ -1,4 +1,5 @@
 from django.test.testcases import TestCase
+from django.urls import reverse
 
 from leavers.tests.views.include import ViewAccessTest
 
@@ -11,3 +12,17 @@ class TestLeaversStartView(ViewAccessTest, TestCase):
 class TestWhoIsLeavingView(ViewAccessTest, TestCase):
     view_name = "who"
     allowed_methods = ["get", "post", "put"]
+
+    def test_me(self):
+        self.client.force_login(self.authenticated_user)
+        response = self.client.post(self.get_url(), {"who_for": "me"})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse("leaver-confirm-details"))
+
+    def test_someone_else(self):
+        self.client.force_login(self.authenticated_user)
+        response = self.client.post(self.get_url(), {"who_for": "someone_else"})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse("report-a-leaver-search"))
--- leavers/tests/views/test_sre.py
+++ leavers/tests/views/test_sre.py
@@ -1,7 +1,10 @@
+from unittest import mock
+
 from django.contrib.auth.models import Group
 from django.test.testcases import TestCase
 
-from leavers.factories import LeavingRequestFactory
+from leavers.factories import LeavingRequestFactory, SlackMessageFactory  # /PS-IGNORE
+from leavers.models import TaskLog
 from leavers.tests.views.include import ViewAccessTest
 
 
@@ -11,10 +14,93 @@ class TestTaskConfirmationView(ViewAccessTest, TestCase):
 
     def setUp(self):
         super().setUp()  # /PS-IGNORE
+        # Create Leaving Request (with initial Slack message)
+        self.leaving_request = LeavingRequestFactory()
+        SlackMessageFactory(leaving_request=self.leaving_request)
+        # Add the SRE User Group (and add the authenticated user to it)  /PS-IGNORE
         sre_group, _ = Group.objects.get_or_create(name="SRE")
         self.authenticated_user.groups.add(sre_group.id)
-        leaving_request = LeavingRequestFactory()
-        self.view_kwargs = {"args": [leaving_request.uuid]}
+        # Update the view_kwargs to pass the leaving_request ID to the view.
+        self.view_kwargs = {"args": [self.leaving_request.uuid]}
+
+    def test_page_contents(self):
+        self.client.force_login(self.authenticated_user)
+        response = self.client.get(self.get_url())
+
+        leaver_first_name = self.leaving_request.leaver_first_name
+        leaver_last_name = self.leaving_request.leaver_last_name
+        self.assertEqual(
+            response.context["leaver_name"], f"{leaver_first_name} {leaver_last_name}"
+        )
+        self.assertEqual(
+            response.context["leaving_date"], self.leaving_request.last_day
+        )
+
+    @mock.patch("leavers.views.sre.send_sre_complete_message")
+    def test_submit_form(self, mock_send_sre_complete_message):
+        self.client.force_login(self.authenticated_user)
+        self.client.post(
+            self.get_url(),
+            {
+                "vpn": True,
+                "govuk_paas": True,
+                "github": True,
+                "sentry": True,
+                "slack": True,
+                "sso": True,
+                "aws": True,
+                "jira": True,
+            },
+        )
+
+        # Check the SRE Complete message is triggered
+        mock_send_sre_complete_message.assert_called_once()
+
+        # Check the task logs are created
+        user_task_logs = TaskLog.objects.filter(
+            user=self.authenticated_user  # /PS-IGNORE
+        )
+        self.assertTrue(user_task_logs.exists())
+        self.assertTrue(
+            user_task_logs.filter(
+                task_name="VPN access removed",
+            ).exists()
+        )
+        self.assertTrue(
+            user_task_logs.filter(
+                task_name="GOV.UK PAAS access removed",
+            ).exists()
+        )
+        self.assertTrue(
+            user_task_logs.filter(
+                task_name="Github access removed",
+            ).exists()
+        )
+        self.assertTrue(
+            user_task_logs.filter(
+                task_name="Sentry access removed",
+            ).exists()
+        )
+        self.assertTrue(
+            user_task_logs.filter(
+                task_name="Slack access removed",
+            ).exists()
+        )
+        self.assertTrue(
+            user_task_logs.filter(
+                task_name="SSO access removed",  # /PS-IGNORE
+            ).exists()
+        )
+        self.assertTrue(
+            user_task_logs.filter(
+                task_name="AWS access removed",
+            ).exists()
+        )
+        self.assertTrue(
+            user_task_logs.filter(
+                task_name="Jira access removed",
+            ).exists()
+        )
 
 
 class TestThankYouView(ViewAccessTest, TestCase):
@@ -23,5 +109,21 @@ class TestThankYouView(ViewAccessTest, TestCase):
 
     def setUp(self):
         super().setUp()  # /PS-IGNORE
-        leaving_request = LeavingRequestFactory()
-        self.view_kwargs = {"args": [leaving_request.uuid]}
+        # Create Leaving Request (with initial Slack message)
+        self.leaving_request = LeavingRequestFactory()
+        SlackMessageFactory(leaving_request=self.leaving_request)
+        # Add the SRE User Group (and add the authenticated user to it)  /PS-IGNORE
+        sre_group, _ = Group.objects.get_or_create(name="SRE")
+        self.authenticated_user.groups.add(sre_group.id)
+        # Update the view_kwargs to pass the leaving_request ID to the view.
+        self.view_kwargs = {"args": [self.leaving_request.uuid]}
+
+    def test_page_contents(self):
+        self.client.force_login(self.authenticated_user)
+        response = self.client.get(self.get_url())
+
+        leaver_first_name = self.leaving_request.leaver_first_name
+        leaver_last_name = self.leaving_request.leaver_last_name
+        self.assertEqual(
+            response.context["leaver_name"], f"{leaver_first_name} {leaver_last_name}"
+        )
