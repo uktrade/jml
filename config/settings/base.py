4f92adc3687153515bd7b2e23ab95f25f0118896
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2022-01-26 14:36:52 +0000

    Remove django_settings_export context processor

--- config/settings/base.py
+++ config/settings/base.py
@@ -56,7 +56,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "django_settings_export.settings_export",
                 "dev_tools.views.dev_tools_context",
                 "core.context_processors.global_context",
             ]
