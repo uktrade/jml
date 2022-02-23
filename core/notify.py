73ab66cfe4dbfd797354258e7e9a3daf154056ac
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2022-02-23 12:56:14 +0000

    Add CSU4 leaver notification email

--- core/notify.py
+++ core/notify.py
@@ -1,15 +1,27 @@
+from enum import Enum
+from typing import Dict
+
 from django.conf import settings
 from notifications_python_client.notifications import NotificationsAPIClient
+
+
+class EmailTemplates(Enum):
+    """
+    GOV.UK Notify email Templates.
+    """
+
+    csu4_leaver_email = "csu4_leaver_email_tempalte_id"
+
 
 notification_client = NotificationsAPIClient(
     settings.GOVUK_NOTIFY_API_KEY,
 )
 
 
-def email(email_address, template_id, personalisation):
+def email(email_address: str, template_id: EmailTemplates, personalisation: Dict):
     message_response = notification_client.send_email_notification(
         email_address=email_address,
-        template_id=template_id,
+        template_id=template_id.value,
         personalisation=personalisation,
     )
 
--- leavers/utils.py
+++ leavers/utils.py
@@ -1,6 +1,9 @@
 from typing import TYPE_CHECKING
 
+from django.conf import settings
+
 from activity_stream.models import ActivityStreamStaffSSOUser
+from core import notify
 from leavers.models import LeavingRequest
 
 if TYPE_CHECKING:
@@ -20,3 +23,24 @@ def update_or_create_leaving_request(
         defaults=defaults,
     )
     return leaving_request
+
+
+def send_csu4_leaver_email(leaving_request: LeavingRequest):
+    """
+    Send Cluster 4 Email to notify of a new leaver.
+
+    The data sent in this email is Sensitive and Personal.
+    Only add more information to this email if it is absolutely necessary.
+
+    Currently included data:
+    - Leaver Name
+    """
+
+    if not settings.CSU4_EMAIL:
+        raise ValueError("CSU4_EMAIL is not set")
+
+    notify.email(
+        email_address=settings.CSU4_EMAIL,
+        template_id=notify.EmailTemplates.csu4_leaver_email,
+        personalisation={"leaver_name": leaving_request.leaver_name},
+    )
