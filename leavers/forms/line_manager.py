1a42f6c75c296e27c40c739385dec3db4c0fe458
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2022-04-19 16:08:32 +0100

    JMLL-737 Update Line Manager security info page

--- leavers/forms/line_manager.py
+++ leavers/forms/line_manager.py
@@ -37,16 +37,16 @@ class UksbsPdfForm(GovFormattedForm):
 
 class LineManagerDetailsForm(GovFormattedForm):
     security_clearance = forms.ChoiceField(
-        label="Security clearance",
+        label="",
         choices=(
             [(None, "Select security clearance type")] + SecurityClearance.choices  # type: ignore
         ),
     )
     holds_government_procurement_card = YesNoField(
-        label="Do they have a Government Procurement Card?",
+        label="",
     )
     rosa_user = YesNoField(
-        label="Do they have ROSA kit?",
+        label="",
     )
 
     def __init__(self, *args, **kwargs):
@@ -54,9 +54,33 @@ class LineManagerDetailsForm(GovFormattedForm):
 
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            Field("security_clearance"),
-            Field.radios("holds_government_procurement_card", inline=True),
-            Field.radios("rosa_user", inline=True),
+            Fieldset(
+                Field.select("security_clearance"),
+                legend="Security clearance",
+                legend_size=Size.MEDIUM,
+            ),
+            Fieldset(
+                HTML(
+                    "<p class='govuk-body'>Government procurement card is a "
+                    "payment card issued by DIT.</p>"
+                ),
+                Field.radios(
+                    "holds_government_procurement_card",
+                    inline=True,
+                ),
+                legend="Do they have a Government Procurement Card?",
+                legend_size=Size.MEDIUM,
+            ),
+            Fieldset(
+                HTML(
+                    "<p class='govuk-body'>ROSA is a secure IT system platform for "
+                    "managing work classified at official, sensitive, secret or top "
+                    "secret level.</p>"
+                ),
+                Field.radios("rosa_user", inline=True),
+                legend="Do they have ROSA kit?",
+                legend_size=Size.MEDIUM,
+            ),
             Submit("submit", "Save and continue"),
         )
 
