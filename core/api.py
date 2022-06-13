fa51891f0a691bd37995fa0e0898648418e2897c
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2022-06-13 15:42:30 +0100

    Convert to DRF and add Hawk authentication

--- core/api.py
+++ core/api.py
@@ -3,19 +3,33 @@ from typing import Optional
 
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
+from django.utils.decorators import decorator_from_middleware
+from django_hawk.middleware import HawkResponseMiddleware
+from django_hawk.utils import DjangoHawkAuthenticationFailed, authenticate_request
+from rest_framework.views import APIView
 
 from activity_stream.models import ActivityStreamStaffSSOUser
 from core.people_finder.interfaces import PersonDetail
 from core.people_finder.utils import index_people_finder_result
 
 
-def people_finder_update(request: HttpRequest) -> HttpResponse:
-    """
-    Update the people finder data.
-    """
-    if request.method == "POST":
+class PeopleFinderUpdateView(APIView):
+    @decorator_from_middleware(HawkResponseMiddleware)
+    def post(self, request: HttpRequest) -> HttpResponse:
+        """
+        Update the people finder data.
+        """
+
+        # Try to authenticate with HAWK
+        try:
+            authenticate_request(request=request)
+        except DjangoHawkAuthenticationFailed:
+            return HttpResponse(status=401)
+
+        # Get the data
         people_finder_data: PersonDetail = json.loads(request.body)
         people_finder_email: Optional[str] = people_finder_data.get("email")
+
         # Validate the data
         if not people_finder_email:
             return HttpResponse(
@@ -50,5 +64,5 @@ def people_finder_update(request: HttpRequest) -> HttpResponse:
             # someone is updated in People Finder that doesn't exist in the
             # staff index.
             pass
+
         return HttpResponse("OK", status=200)
-    return HttpResponse(status=400)
--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 
-from core.api import people_finder_update
+from core.api import PeopleFinderUpdateView
 
 urlpatterns = [
     path("healthcheck/", include("core.health_check.urls")),
     path(
-        "api/people-finder/update/", people_finder_update, name="people-finder-update"
+        "api/people-finder/update/",
+        PeopleFinderUpdateView.as_view(),
+        name="people-finder-update",
     ),
 ]
