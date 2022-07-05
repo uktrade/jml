656a587b415d0c1d616d369e9967dcbfdfddbd84
Ross Miller <github@decodedcollective.com>
2022-07-05 08:59:10 +0100

    Fix issues with SSO

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,6 @@ from django.urls import include, path
 from core.utils.urls import decorate_urlpatterns
 
 private_urlpatterns = [
-    path("auth/", include("authbroker_client.urls", namespace="authbroker")),
     path("admin/", admin.site.urls),
     path("assets/", include("asset_registry.urls")),
     path("leavers/", include("leavers.urls")),
@@ -15,6 +14,7 @@ private_urlpatterns = decorate_urlpatterns(private_urlpatterns, login_required)
 
 public_url_patterns = [
     path("dev-tools/", include("dev_tools.urls")),
+    path("auth/", include("authbroker_client.urls", namespace="authbroker")),
     path("dit-activity-stream/", include("dit_activity_stream.urls")),
     path("healthcheck/", include("core.health_check.urls")),
 ]
