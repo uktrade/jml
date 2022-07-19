16dbda331bcb37625dec623c5e42f3e5a4b9eec6
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2022-07-19 16:37:39 +0100

    JMLL-863 Create api endpoint for People Finder Webhook caller

--- config/urls.py
+++ config/urls.py
@@ -5,19 +5,17 @@ from django.urls import include, path
 from core.utils.urls import decorate_urlpatterns
 
 private_urlpatterns = [
-    path("", include("core.landing_pages.urls")),
     path("admin/", admin.site.urls),
     path("assets/", include("asset_registry.urls")),
     path("leavers/", include("leavers.urls")),
-    path("cookie/", include("core.cookies.urls")),
 ]
 private_urlpatterns = decorate_urlpatterns(private_urlpatterns, login_required)
 
 public_url_patterns = [
+    path("", include("core.urls")),
     path("dev-tools/", include("dev_tools.urls")),
     path("auth/", include("authbroker_client.urls", namespace="authbroker")),
     path("dit-activity-stream/", include("dit_activity_stream.urls")),
-    path("healthcheck/", include("core.health_check.urls")),
 ]
 
 urlpatterns = private_urlpatterns + public_url_patterns
--- core/people_finder/interfaces.py
+++ core/people_finder/interfaces.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from typing import Iterable, Optional, TypedDict
+from dataclasses import dataclass
+from typing import Iterable, Optional
 
 from core.people_finder.client import (
     FailedToGetPeopleRecords,
@@ -8,67 +9,68 @@ from core.people_finder.client import (
 )
 
 
-class Person(TypedDict):
+@dataclass
+class PersonDetail:
     first_name: str
     last_name: str
     job_title: str
     directorate: str
     email: str
-    phone: str
-    grade: str
+    phone: Optional[str]
+    grade: Optional[str]
     photo: Optional[str]
     photo_small: Optional[str]
 
 
 class PeopleFinderBase(ABC):
     @abstractmethod
-    def get_details(self, *, sso_legacy_user_id: str) -> Person:
+    def get_details(self, *, sso_legacy_user_id: str) -> PersonDetail:
         raise NotImplementedError
 
     @abstractmethod
-    def get_all(self) -> Iterable[Person]:
+    def get_all(self) -> Iterable[PersonDetail]:
         raise NotImplementedError
 
 
 class PeopleFinderStubbed(PeopleFinderBase):
-    def get_details(self, sso_legacy_user_id: str) -> Person:
-        return {
-            "first_name": "Joe",  # /PS-IGNORE
-            "last_name": "Bloggs",
-            "job_title": "Job title",
-            "directorate": "Directorate name",
-            "email": "joe.bloggs@example.com",  # /PS-IGNORE
-            "phone": "0123456789",
-            "grade": "Example Grade",
-            "photo": "",
-            "photo_small": "",
-        }
+    def get_details(self, sso_legacy_user_id: str) -> PersonDetail:
+        return PersonDetail(
+            first_name="Joe",  # /PS-IGNORE
+            last_name="Bloggs",
+            job_title="Job title",
+            directorate="Directorate name",
+            email="joe.bloggs@example.com",  # /PS-IGNORE
+            phone="0123456789",
+            grade="Example Grade",
+            photo="",
+            photo_small="",
+        )
 
-    def get_all(self) -> Iterable[Person]:
+    def get_all(self) -> Iterable[PersonDetail]:
         return iter(
             [
-                {
-                    "first_name": "Joe",  # /PS-IGNORE
-                    "last_name": "Bloggs",
-                    "job_title": "Job title",
-                    "directorate": "Directorate name",
-                    "email": "joe.bloggs@example.com",  # /PS-IGNORE
-                    "phone": "0123456789",
-                    "grade": "Example Grade",
-                    "photo": "",
-                    "photo_small": "",
-                },
-                {
-                    "first_name": "Jane",  # /PS-IGNORE
-                    "last_name": "Doe",
-                    "job_title": "Job title",
-                    "directorate": "Directorate name",
-                    "email": "jane.doe@example.com",  # /PS-IGNORE
-                    "phone": "0987654321",
-                    "grade": "Example Grade",
-                    "photo": "",
-                    "photo_small": "",
-                },
+                PersonDetail(
+                    first_name="Joe",  # /PS-IGNORE
+                    last_name="Bloggs",
+                    job_title="Job title",
+                    directorate="Directorate name",
+                    email="joe.bloggs@example.com",  # /PS-IGNORE
+                    phone="0123456789",
+                    grade="Example Grade",
+                    photo="",
+                    photo_small="",
+                ),
+                PersonDetail(
+                    first_name="Jane",  # /PS-IGNORE
+                    last_name="Doe",
+                    job_title="Job title",
+                    directorate="Directorate name",
+                    email="jane.doe@example.com",  # /PS-IGNORE
+                    phone="0987654321",
+                    grade="Example Grade",
+                    photo="",
+                    photo_small="",
+                ),
             ]
         )
 
@@ -78,7 +80,7 @@ class PeopleFinderPersonNotFound(Exception):
 
 
 class PeopleFinder(PeopleFinderBase):
-    def get_details(self, sso_legacy_user_id: str) -> Person:
+    def get_details(self, sso_legacy_user_id: str) -> PersonDetail:
         try:
             person = get_details(sso_legacy_user_id)
         except FailedToGetPeopleRecords:
@@ -91,17 +93,17 @@ class PeopleFinder(PeopleFinderBase):
             job_title = person["roles"][0]["role"]
             directorate = person["roles"][0]["team_name"]
 
-        return {
-            "first_name": person["first_name"],
-            "last_name": person["last_name"],
-            "job_title": job_title,
-            "directorate": directorate,
-            "email": person["email"],
-            "phone": person["primary_phone_number"],
-            "grade": person["grade"],
-            "photo": person["photo"],
-            "photo_small": person["photo_small"],
-        }
+        return PersonDetail(
+            first_name=person["first_name"],
+            last_name=person["last_name"],
+            job_title=job_title,
+            directorate=directorate,
+            email=person["email"],
+            phone=person["primary_phone_number"],
+            grade=person["grade"],
+            photo=person["photo"],
+            photo_small=person["photo_small"],
+        )
 
-    def get_all(self) -> Iterable[Person]:
+    def get_all(self) -> Iterable[PersonDetail]:
         return PeopleFinderIterator()
