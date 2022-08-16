be6627a6bf9236582fe7b9e0820956bee92b4eb9
Ross Miller <ross.miller@digital.trade.gov.uk>
2022-08-16 11:07:50 +0100

    Make privilege elevation optional

--- nessus/management/commands/setup_test_user.py
+++ nessus/management/commands/setup_test_user.py
@@ -6,8 +6,17 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = "Create Nessus test user"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--elevate",
+            help="Should the test users privileges be elevated?",
+            dest="elevate",
+        )
+
     def handle(self, *args, **options):
         if hasattr(settings, "NESSUS_TEST_ENABLED") and settings.NESSUS_TEST_ENABLED:
+            elevate = options["elevate"]
+
             User = get_user_model()
 
             user, _ = User.objects.update_or_create(**settings.NESSUS_TEST_USER)
@@ -18,15 +27,16 @@ class Command(BaseCommand):
                 self.style.SUCCESS("Successfully created/updated Nessus test user")
             )
 
-            user.is_superuser = True
-            user.is_staff = True
-            user.save()
+            if elevate:
+                user.is_superuser = True
+                user.is_staff = True
+                user.save()
 
-            self.stdout.write(
-                self.style.SUCCESS(
-                    "Successfully elevated privileges for Nessus test user"
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        "Successfully elevated privileges for Nessus user"
+                    )
                 )
-            )
         else:
             self.stdout.write(
                 self.style.ERROR(
