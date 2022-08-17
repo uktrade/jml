ae96d18feaa17c26217206d44439bcbc3d8ff008
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2022-08-17 13:51:37 +0100

    Comment out all celery

--- core/tasks.py
+++ core/tasks.py
@@ -1,7 +1,7 @@
-from django.db.models.query import QuerySet
-from django_workflow_engine.exceptions import WorkflowNotAuthError
-from django_workflow_engine.executor import WorkflowExecutor
-from django_workflow_engine.models import Flow
+# from django.db.models.query import QuerySet
+# from django_workflow_engine.exceptions import WorkflowNotAuthError
+# from django_workflow_engine.executor import WorkflowExecutor
+# from django_workflow_engine.models import Flow
 
 # from activity_stream.utils import ingest_activity_stream
 from config.celery import celery_app
@@ -18,26 +18,26 @@ def debug_task(self):
     print("Request: {0!r}".format(self.request))
 
 
-@celery_app.task(bind=True)
-def progress_workflows(self):
-    logger.info("RUNNING progress_workflows")
-    in_progress_flows: QuerySet[Flow] = Flow.objects.filter(finished__isnull=True)
-    for flow in in_progress_flows:
-        progress_workflow.delay(flow_pk=flow.pk)
-    logger.info(f"Triggered {in_progress_flows.count()} workflows")
+# @celery_app.task(bind=True)
+# def progress_workflows(self):
+#     logger.info("RUNNING progress_workflows")
+#     in_progress_flows: QuerySet[Flow] = Flow.objects.filter(finished__isnull=True)
+#     for flow in in_progress_flows:
+#         progress_workflow.delay(flow_pk=flow.pk)
+#     logger.info(f"Triggered {in_progress_flows.count()} workflows")
 
 
-@celery_app.task(bind=True)
-def progress_workflow(self, flow_pk: str):
-    logger.info(f"RUNNING progress_workflow {flow_pk=}")
-    # Get workflow from task
-    flow: Flow = Flow.objects.get(pk=flow_pk)
-    if not flow.is_complete:
-        executor = WorkflowExecutor(flow)
-        try:
-            executor.run_flow(user=None)
-        except WorkflowNotAuthError as e:
-            logger.warning(f"{e}")
+# @celery_app.task(bind=True)
+# def progress_workflow(self, flow_pk: str):
+#     logger.info(f"RUNNING progress_workflow {flow_pk=}")
+#     # Get workflow from task
+#     flow: Flow = Flow.objects.get(pk=flow_pk)
+#     if not flow.is_complete:
+#         executor = WorkflowExecutor(flow)
+#         try:
+#             executor.run_flow(user=None)
+#         except WorkflowNotAuthError as e:
+#             logger.warning(f"{e}")
 
 
 # @celery_app.task(bind=True)
