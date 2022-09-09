1aca36a1c8ae60082a9c76d8570b3c92c668d6df
Ross Miller <ross.miller@digital.trade.gov.uk>
2022-09-09 09:38:44 +0100

    Switch from Zendesk to abstracted helpdesk

--- core/utils/lsd.py
+++ core/utils/lsd.py
@@ -1,21 +1,22 @@
 import logging
 
 from django.conf import settings
-from zenpy import Zenpy
-from zenpy.lib.api_objects import Ticket
+from helpdesk_client import get_helpdesk_interface
+from helpdesk_client.interfaces import (
+    HelpDeskComment,
+    HelpDeskTicket,
+    Priority,
+    Status,
+    TicketType,
+)
 
 from leavers.models import LeavingRequest
 
 logger = logging.getLogger(__name__)
 
 
-def get_zendesk_client() -> Zenpy:
-    creds = {
-        "email": settings.LSD_ZENDESK_EMAIL,
-        "token": settings.LSD_ZENDESK_TOKEN,
-        "subdomain": settings.LSD_ZENDESK_SUBDOMAIN,
-    }
-    return Zenpy(**creds)
+helpdesk_interface = get_helpdesk_interface(settings.HELP_DESK_INTERFACE)
+helpdesk = helpdesk_interface(credentials=settings.HELP_DESK_CREDS)
 
 
 def inform_lsd_team_of_leaver(leaving_request: LeavingRequest):
@@ -36,24 +37,26 @@ def inform_lsd_team_of_leaver(leaving_request: LeavingRequest):
         logger.warning(f"Creating zendesk ticket for LSD team regarding {leaver_name}")
         return None
 
-    client = get_zendesk_client()
-    # Create a Zendesk Ticket /PS-IGNORE
-    client.tickets.create(
-        Ticket(
+    # Create a helpdesk /PS-IGNORE
+    comment_body = (
+        "We have been informed that the following person is "
+        "leaving/has left the department.\n"
+        f"Name: {leaver_name}\n"
+        f"Email: {leaver_email}\n"
+        f"Date of Leaving: {leaving_date_str}\n"
+        "Please ensure that permissions are removed for this user "
+        "(where appropriate. SSO, Datahub, Digital Worskspace, OKTA)."
+    )
+
+    helpdesk.create_ticket(
+        HelpDeskTicket(
             subject=f"Notification of Leaver: {leaver_name}",
-            comment=(
-                "We have been informed that the following person is "
-                "leaving/has left the department.\n"
-                f"Name: {leaver_name}\n"
-                f"Email: {leaver_email}\n"
-                f"Date of Leaving: {leaving_date_str}\n"
-                "Please ensure that permissions are removed for this user "
-                "(where appropriate. SSO, Datahub, Digital Worskspace, OKTA)."
+            comment=HelpDeskComment(
+                body=comment_body,
             ),
-            priority="normal",
-            type="task",
-            status="new",
-            requester=client.users.me(),
+            priority=Priority.NORMAL,
+            ticket_type=TicketType.TASK,
+            status=Status.NEW,
         )
     )
 
