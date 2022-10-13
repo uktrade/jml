f76e20c261cfe29e061210dd326325303c0b3475
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2022-10-13 09:34:38 +0100

    JMLL-1128 Move the project over to poetry
    I came across a couple of issues along the way and have done the
    following to fix them:
    * add a database router for people_data
    * improve the dockerfile
    * bump the runtime to python 3.9.x (was fixed at 3.9.12)
    * add more information to the readme

---
+++ core/people_data/routers.py
@@ -0,0 +1,6 @@
+class PeopleDataRouter:
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if db == "people_data":
+            return False
+        
+        return None
