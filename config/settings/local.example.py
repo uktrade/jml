70ddcb1bb181f8614413f5e2557e2c31a669fdfb
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2022-10-18 15:59:23 +0100

    Add people_data local configuration

--- config/settings/local.example.py
+++ config/settings/local.example.py
@@ -30,6 +30,14 @@ LOGGING["loggers"] = {
     },
 }
 
+DATABASES["people_data"] = {
+    "HOST": env("PEOPLE_DATA_POSTGRES_HOST"),
+    "NAME": env("PEOPLE_DATA_POSTGRES_DATABASE"),
+    "PORT": env("PEOPLE_DATA_POSTGRES_PORT", default=5432),
+    "ENGINE": "django.db.backends.postgresql",
+    "USER": env("PEOPLE_DATA_POSTGRES_USERNAME"),
+    "PASSWORD": env("PEOPLE_DATA_POSTGRES_PASSWORD"),
+}
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
