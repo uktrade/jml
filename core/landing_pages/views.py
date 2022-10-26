196aa003a008b0d79f4a0135e8cb56b22610e2aa
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2022-10-26 14:50:32 +0100

    Update context definitions

--- core/landing_pages/views.py
+++ core/landing_pages/views.py
@@ -3,29 +3,26 @@ from django.template.response import TemplateResponse
 
 
 def contact_us_page(request):
-    context = {}
-    context.update(
-        page_title="Contact us",
-        JML_TEAM_CONTACT_EMAIL=settings.JML_TEAM_CONTACT_EMAIL,
-    )
+    context = {
+        "page_title": "Contact us",
+        "JML_TEAM_CONTACT_EMAIL": settings.JML_TEAM_CONTACT_EMAIL,
+    }
     return TemplateResponse(request, "landing_pages/contact_us_page.html", context)
 
 
 def leaver_landing_page(request):
-    context = {}
-    context.update(
-        page_title="Leavers and Line Managers",
-        JML_TEAM_CONTACT_EMAIL=settings.JML_TEAM_CONTACT_EMAIL,
-    )
+    context = {
+        "page_title": "Leavers and Line Managers",
+        "JML_TEAM_CONTACT_EMAIL": settings.JML_TEAM_CONTACT_EMAIL,
+    }
     return TemplateResponse(request, "landing_pages/leaver_landing_page.html", context)
 
 
 def data_processor_landing_page(request):
-    context = {}
-    context.update(
-        page_title="Data Processors",
-        JML_TEAM_CONTACT_EMAIL=settings.JML_TEAM_CONTACT_EMAIL,
-    )
+    context = {
+        "page_title": "Data Processors",
+        "JML_TEAM_CONTACT_EMAIL": settings.JML_TEAM_CONTACT_EMAIL,
+    }
     return TemplateResponse(
         request, "landing_pages/data_processor_landing_page.html", context
     )
