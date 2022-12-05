2f032b0a6b28d73576eb6e446d12034793098d78
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2022-12-05 13:35:25 +0000

    Remove test-error paths

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,5 @@
 from typing import List, Union
 
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.urls import include, path
 from django.urls.resolvers import URLPattern, URLResolver
@@ -25,26 +24,3 @@ urlpatterns: List[Union[URLPattern, URLResolver]] = (
     private_urlpatterns + public_url_patterns
 )
 urlpatterns = decorate_urlpatterns(urlpatterns, never_cache)
-
-if settings.APP_ENV == "dev":
-    from django.http import HttpRequest, HttpResponse
-    from django.shortcuts import render
-
-    def error_400(request: HttpRequest) -> HttpResponse:
-        return render(request, "400.html", status=400)
-
-    def error_403(request: HttpRequest) -> HttpResponse:
-        return render(request, "403.html", status=403)
-
-    def error_404(request: HttpRequest) -> HttpResponse:
-        return render(request, "404.html", status=404)
-
-    def error_500(request: HttpRequest) -> HttpResponse:
-        return render(request, "500.html", status=500)
-
-    urlpatterns += [
-        path("test-error/400/", error_400),
-        path("test-error/403/", error_403),
-        path("test-error/404/", error_404),
-        path("test-error/500/", error_500),
-    ]
