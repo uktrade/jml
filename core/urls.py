af38a1ffcd5e0247322cce0ecb5617b4182852b0
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2022-12-08 14:01:35 +0000

    JMLL-1210 Add custom error pages

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
+from typing import List, Union
+
 from django.contrib.auth.decorators import login_required
 from django.urls import include, path
+from django.urls.resolvers import URLPattern, URLResolver
 from django.views.decorators.cache import never_cache
 
 from core.utils.urls import decorate_urlpatterns
 
-private_urlpatterns = [
+private_urlpatterns: List[Union[URLPattern, URLResolver]] = [
     path("", include("core.landing_pages.urls")),
     path("activity-stream/", include("activity_stream.urls")),
     path("cookie/", include("core.cookies.urls")),
@@ -13,9 +16,11 @@ private_urlpatterns = [
 ]
 private_urlpatterns = decorate_urlpatterns(private_urlpatterns, login_required)
 
-public_url_patterns = [
+public_url_patterns: List[Union[URLPattern, URLResolver]] = [
     path("healthcheck/", include("core.health_check.urls")),
 ]
 
-urlpatterns = private_urlpatterns + public_url_patterns
+urlpatterns: List[Union[URLPattern, URLResolver]] = (
+    private_urlpatterns + public_url_patterns
+)
 urlpatterns = decorate_urlpatterns(urlpatterns, never_cache)
