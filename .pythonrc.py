dd77d45e6bb186706b3728ab8effc0eb93e91818
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2022-12-08 15:22:14 +0000

    JMLL-1235 Add debug functions to pythonrc

---
+++ .pythonrc.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+from django.db import connections
+
+from activity_stream.models import ActivityStreamStaffSSOUser
+from core.people_data.interfaces import row_to_dict
+
+
+def get_sso_user_by_email(email: str) -> ActivityStreamStaffSSOUser:
+    return ActivityStreamStaffSSOUser.objects.get(sso_emails__email_address=email)
+
+
+def query_people_data_by_email(email: str) -> list[dict[str, Any]]:
+    with connections["people_data"].cursor() as cursor:
+        cursor.execute(
+            "SELECT * FROM dit.people_data__jml WHERE email_address = %s", [email]
+        )
+        rows = cursor.fetchall()
+
+        return [row_to_dict(cursor=cursor, row=row) for row in rows]
