3c22e4e75ce492475fded1ae8a7a3a98388f4d34
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2022-12-16 13:19:12 +0000

    Remove unused imports

--- core/accessibility/tests/test_views.py
+++ core/accessibility/tests/test_views.py
@@ -1,9 +1,6 @@
-from django.http import SimpleCookie
-from django.http.response import HttpResponse
 from django.test import TestCase
 from django.urls import reverse_lazy
 
-from core.cookies.views import COOKIE_KEY
 from user.test.factories import UserFactory
 
 
