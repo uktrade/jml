7f5e5e88639e677b6219d86bfbb8e08356075614
Luisella Strona <luisella21@hotmail.com>
2022-12-20 18:12:16 +0000

    Added celery task

--- leavers/tasks.py
+++ leavers/tasks.py
@@ -1,19 +1,19 @@
+from celery import shared_task
 from datetime import date
 from leavers.models import LeavingRequest
 
-from leavers.utils.workday_calculation import (
-    is_date_within_payroll_cutoff_interval,
-)
+from leavers.utils.workday_calculation import is_date_within_payroll_cutoff_interval
 
+
+@shared_task
 def task_notify_hr():
-    is_within, cut_off_date =  is_date_within_payroll_cutoff_interval(date.today())
+    is_within, cut_off_date = is_date_within_payroll_cutoff_interval(date.today())
     if is_within:
         # Check if there are incomplete leavers with leaving date
         # before or on the cutoff date
         leavers_incomplete_qs = LeavingRequest.objects.filter(
-            line_manager_complete__isnull=True,
-            leaving_date__date__lte=cut_off_date
+            line_manager_complete__isnull=True, leaving_date__date__lte=cut_off_date
         )
         if leavers_incomplete_qs.count():
-        # Email the list to HR
+            # Email the list to HR
             pass
