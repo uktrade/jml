8dc75314fe0b2d4d2f492236e105edcd9cd392f4
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2023-01-05 11:04:16 +0000

    Fix migrations

--- leavers/migrations/0077_alter_leavingrequest_annual_number_and_more.py
+++ leavers/migrations/0077_alter_leavingrequest_annual_number_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("leavers", "0075_update_task_info"),
+        ("leavers", "0076_update_flows"),
     ]
 
     operations = [
