f00881c7f8b4dee697050617f3f6aea5984f3aea
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2023-02-10 16:01:16 +0000

    JMLL-1276 Add remember search and clear search functionality

--- core/templatetags/core.py
+++ core/templatetags/core.py
@@ -1,4 +1,5 @@
 from django import template
+from django.template.defaulttags import URLNode, url
 
 from core.utils.helpers import make_possessive
 
@@ -17,3 +18,32 @@ def get_current_url_with_query_params(context, **kwargs):
     for key, value in kwargs.items():
         query_params[key] = value
     return request.path + "?" + query_params.urlencode()
+
+
+@register.simple_tag(takes_context=True)
+def get_current_url_without_query_params(context, **kwargs):
+    request = context["request"]
+    return request.path
+
+
+class URLNodeWithUrlQuery(URLNode):
+    def render(self, context) -> str:
+        request = context["request"]
+        query_params = request.GET.copy()
+        return super().render(context) + "?" + query_params.urlencode()
+
+    @classmethod
+    def from_url_node(cls, url_node: URLNode):
+        return cls(
+            view_name=url_node.view_name,
+            args=url_node.args,
+            kwargs=url_node.kwargs,
+            asvar=url_node.asvar,
+        )
+
+
+@register.tag
+def url_with_query_params(parser, token):
+    url_node = url(parser, token)
+    custom_url_node = URLNodeWithUrlQuery.from_url_node(url_node)
+    return custom_url_node
