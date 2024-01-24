baac5b93e4225a1064cc9cadda8fa793a03753eb
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2024-01-24 10:53:41 +0000

    Add location to admin

--- core/beis_service_now/admin.py
+++ core/beis_service_now/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from core.beis_service_now.models import (
     ServiceNowAsset,
     ServiceNowDirectorate,
+    ServiceNowLocation,
     ServiceNowUser,
 )
 
 admin.site.register(ServiceNowAsset)
 admin.site.register(ServiceNowUser)
 admin.site.register(ServiceNowDirectorate)
+admin.site.register(ServiceNowLocation)
