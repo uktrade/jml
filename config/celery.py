fcdb191fcbd31464a1d3b9de5e53ea527a0b487f
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2024-04-10 15:07:44 +0100

    DWPF-1523 Update health checks for DBT Platform (#439)
    Update current healthchecks, add pingdom/ping.xml and celery_app
    healthcheck for dbt platform.
    
    - [x] JIRA ticket referenced in title
    - [x] Title is clear and concise
    - [x] Description gives any relevant detail
    - [x] Tests are up to date
    - [x] Documentation is up to date
    
    ---------
    
    Co-authored-by: Cameron Lamb <cameron.lamb@digital.trade.gov.uk>

--- config/celery.py
+++ config/celery.py
@@ -1,10 +1,12 @@
+from dbt_copilot_python.celery_health_check import healthcheck
+
 from celery import Celery
 from celery.schedules import crontab
 
 celery_app = Celery("DjangoCelery")
+celery_app = healthcheck.setup(celery_app)
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
 celery_app.autodiscover_tasks()
-
 
 celery_app.conf.beat_schedule = {
     # Queue up the workflows every 5 minutes.
