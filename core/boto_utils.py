7c36ee924dd7d2fcfc7c51ef4fb5b135f90ba698
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2024-12-10 17:19:42 +0000

    Add empty aws access details for local use

--- core/boto_utils.py
+++ core/boto_utils.py
@@ -10,13 +10,17 @@ logger = logging.getLogger(__name__)
 def get_s3_resource():
     if settings.S3_LOCAL_ENDPOINT_URL:
         logger.debug("using local S3 endpoint %s", settings.S3_LOCAL_ENDPOINT_URL)
-        return boto3.resource("s3", endpoint_url=settings.S3_LOCAL_ENDPOINT_URL)
+        return boto3.resource(
+            "s3",
+            endpoint_url=settings.S3_LOCAL_ENDPOINT_URL,
+            aws_access_key_id="",
+            aws_secret_access_key="",
+        )
 
     return boto3.resource("s3")
 
 
 def get_s3_client():
-
     if settings.S3_LOCAL_ENDPOINT_URL:
         logger.debug("using local S3 endpoint %s", settings.S3_LOCAL_ENDPOINT_URL)
         return boto3.client("s3", endpoint_url=settings.S3_LOCAL_ENDPOINT_URL)
