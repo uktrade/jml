270507fe0a1c7cce912453a25d6928f54e95d6be
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2024-12-12 12:56:56 +0000

    Add example files to the local s3 bucket

--- docker/.localstack/init/buckets.sh
+++ docker/.localstack/init/buckets.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -x
+
 awslocal s3 mb s3://jml.local
+echo "jml.local S3 bucket created"
+
+awslocal s3 cp /examples/people_data.jsonl s3://jml.local/data-flow/exports/local-development/ExportPeopleDataNewIdentityPipeline/20241106T143632.jsonl
+echo "People Data file added to bucket"
+awslocal s3 cp /examples/staff_sso.jsonl s3://jml.local/data-flow/exports/local-development/StaffSSOUsersPipeline/20241106T000000/full_ingestion.jsonl
+echo "Staff SSO file added to bucket"
 
 set +x
 echo "S3 Configured"
