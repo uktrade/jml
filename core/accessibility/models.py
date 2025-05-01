b280a129e6a0fba65f55cd0fca71a2fee4bdb146
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-05-01 14:15:17 +0100

    Add rich text model for Accessibility statement

---
+++ core/accessibility/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django_prose_editor.fields import ProseEditorField
+
+
+class AccessibilityStatement(models.Model):
+    content = ProseEditorField(
+        extensions={
+            "Bold": True,
+            "Italic": True,
+            "BulletList": True,
+            "Link": True,
+        },
+        sanitize=True,
+    )
