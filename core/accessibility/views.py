3e623221011138c4a52c52806a73f702edd17947
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-05-01 15:11:53 +0100

    Fix formatting

--- core/accessibility/views.py
+++ core/accessibility/views.py
@@ -3,11 +3,9 @@ from django.template.response import TemplateResponse
 from core.accessibility.utils import get_accessibility_statement
 
 
-
-
 def accessibility_statement(request):
     context = {
         "page_title": "Accessibility statement",
-        "accessibility_statement": get_accessibility_statement()
+        "accessibility_statement": get_accessibility_statement(),
     }
     return TemplateResponse(request, "accessibility/statement.html", context)
